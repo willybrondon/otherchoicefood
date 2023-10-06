75e22cf0fe57ee94a8cddfae1224f985a72c6657
willybrondon <ndanguewilly@gmail.com>
2023-10-06 13:24:26 +0200

    feat vendor registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import  UserForm
-from .models import User
+from .models import User, UserProfile
 from django.contrib import messages
+from vendor.forms import VendorForm
 # Create your views here.
 
 def registerUser(request):
@@ -36,3 +37,34 @@ def registerUser(request):
         'form':form,
         }
     return render(request, 'accounts/registerUser.html', context)
+
+def registerVendor(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        v_form = VendorForm(request.POST, request.FILES)
+        if form.is_valid() and v_form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            password = form.cleaned_data['password']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            user = User.objects.create_user(first_name=first_name,last_name=last_name, username=username, password=password,email=email)
+            user.role = user.VENDOR
+            user.save()
+            vendor = v_form.save(commit=False)
+            vendor.user = user 
+            user_profile = UserProfile.objects.get(user=user)
+            vendor.user_profile = user_profile
+            vendor.save()
+            messages.success(request, 'Your account has been regsited sucessfully ! please wait for approval')
+            return redirect('registerVendor')
+        else:
+            print('Invalid data')
+            print(form.errors)
+    else:
+        form = UserForm()
+        v_form = VendorForm()
+    context = {'form':form,
+                'v_form': v_form,}
+    
+    return render(request, 'accounts/registerVendor.html', context)
